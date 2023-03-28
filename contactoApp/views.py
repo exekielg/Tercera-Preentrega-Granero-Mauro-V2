7d08f0afca5d6025efed85488c8c3f256a09605f
exekielg <exekielg@gmail.com>
2023-03-27 23:05:13 -0300

    Finalizacion de contactoApp

--- contactoApp/views.py
+++ contactoApp/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import formcontacto
 
 # Create your views here.
 
 def contacto(request):
         formulario_contacto= formcontacto
+        if request.method=="POST":
+                formulario_contacto=formcontacto(data= request.POST)
+                formulario_contacto.is_valid()
+                nombre=request.POST.get("Nombre")
+                cel=request.POST.get("Celular")
+                email=request.POST.get("Email")
+                mensaje=request.POST.get("Mensaje")
+
+                return redirect("/contacto/?okmensaje")
+
+
+                
+        
+        
+
+
         return render(request, 'contactoApp/contacto.html', {'miformulario':formulario_contacto})
+
 
 
 
