431739dc85f4a98a42bffa74586b2c5831b12302
exekielg <exekielg@gmail.com>
2023-03-27 23:13:18 -0300

    Comentarios de la contactoApp

--- contactoApp/views.py
+++ contactoApp/views.py
@@ -6,14 +6,14 @@ from .forms import formcontacto
 def contacto(request):
         formulario_contacto= formcontacto
         if request.method=="POST":
-                formulario_contacto=formcontacto(data= request.POST)
+                formulario_contacto=formcontacto(data= request.POST) # se guardan los datos del forms en Variables 
                 formulario_contacto.is_valid()
                 nombre=request.POST.get("Nombre")
                 cel=request.POST.get("Celular")
                 email=request.POST.get("Email")
                 mensaje=request.POST.get("Mensaje")
 
-                return redirect("/contacto/?okmensaje")
+                return redirect("/contacto/?okmensaje") #redirecciona a misma pagina contacto pasando por parametro el mensaje de validacion
 
 
                 
